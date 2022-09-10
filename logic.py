70f13e7196ac6f8b178265e74d1435a98611428b
artikov <oybekartikov@icloud.com>
2022-09-10 17:08:17 +0500

    initial

---
+++ logic.py
@@ -0,0 +1,51 @@
+from typing import NamedTuple
+from itertools import cycle
+
+
+class Player(NamedTuple):
+    label: str
+    color: str
+
+
+class Move(NamedTuple):
+    row: int
+    col: int
+    label: str = ""
+
+
+BOARD_SIZE = 3
+DEFAULT_PLAYERS = (
+    Player(label="X", color="blue"),
+    Player(label="O", color="red")
+)
+
+
+class Game:
+    def __init__(self, players=DEFAULT_PLAYERS, board_size=BOARD_SIZE):
+        self._players = cycle(players)
+        self.board_size = board_size
+        self.current_player = next(self._players)
+        self.winner_combo = []
+        self._current_moves = []
+        self._has_winner = False
+        self._winning_combos = []
+        self._setup_board()
+
+    def _setup_board(self):
+        self._current_moves = [
+            [Move(row, col) for col in range(self.board_size)]
+            for row in range(self.board_size)
+        ]
+
+        self._winning_combos = self._get_winning_combos()
+
+    def _get_winning_combos(self):
+        rows = [
+            [(move.row, move.col) for move in row]
+            for row in self._current_moves
+        ]
+
+        columns = [list(col) for col in zip(*rows)]
+        first_diagonal = [row[i] for i, row in enumerate(rows)]
+        second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
+        return rows + columns + [first_diagonal, second_diagonal]
---
+++ main.py
@@ -0,0 +1,10 @@
+from board import Board
+
+
+def main():
+    board = Board()
+    board.mainloop()
+    
+
+if __name__ == "__main__":
+    main()
